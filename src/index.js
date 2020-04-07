e95bab4c8d46c68bdf63632e6fe8f04174f5fd85
Eduardo Filho <c.duduj14@gmail.com>
2020-04-07 12:50:39 -0300

    Desafio Conceitos Nodejs

---
+++ src/index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const { uuid, isUuid} = require('uuidv4');
+const { cors } = require('cors');
+
+const app = express();
+
+app.use(express.json());
+//app.use(cors());
+
+const repositories = [];
+
+  function logRequests(request, response, next){
+      const { method, url } = request;
+
+      const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+      console.time(logLabel);
+
+      next();
+
+      console.timeEnd(logLabel);
+  }
+
+  function validateProjectId(request, response, next){
+    const { id } = request.params;
+
+    if(!isUuid(id)){
+        return response.status(400).json( { error: 'Invadid project ID.'});
+    }
+    return next();
+  }
+  app.use(logRequests);
+  app.use('/repositories/:id', validateProjectId)
+
+ app.get('/repositories', (request, response) => {
+     const { title } = request.query;
+
+    const results = title
+    ? repositories.filter(repositor => repositor.title.includes(title))
+    : repositories; 
+
+    return response.json(results);    
+});
+app.post('/repositories', (request, response) => {
+    const { title, url, techs, likes } = request.body;
+    
+    const repositor = { id: uuid(), title, url, techs, likes};
+
+    repositories.push(repositor);
+
+    return response.json(repositor);    
+});
+app.put('/repositories/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, url, techs, likes } = request.body;
+
+    const repositoriesIndex = repositories.findIndex(repositor => repositor.id === id);
+
+    if(repositoriesIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const repositor = {
+        id,
+        title,
+        url,
+        techs,
+        likes,
+    };
+
+    repositories[repositoriesIndex] = repositor;
+
+    return response.json(repositor);    
+});
+app.delete('/repositories/:id', (request, response) => {
+    const { id }= request.params;
+
+    const repositoriesIndex = repositories.findIndex(repositor => repositor.id === id);
+
+    if(repositoriesIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    repositories.splice(repositoriesIndex, 1);
+
+    
+    return response.status(204).send();    
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+    const { id } = request.params;
+    const { likes } = request.body;
+
+    const repositoriesIndex = repositories.findIndex(repositor => repositor.id === id);
+
+    if(repositoriesIndex < 0){
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+    const repositor = { id: uuid(), likes };
+
+    repositories[repositoriesIndex] = repositor;
+
+    return response.json(repositor); 
+  });
+
+app.listen(3333, () => {
+    console.log('Back-end OK!');
+});
+module.exports = app;
